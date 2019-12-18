55dacded27ffa89549114881d8f707dac8ae8936
RomainPestre <rompste@gmail.com>
2019-12-18 17:05:36 +0100

    Functional Navigation Bar

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.movieapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.nav_toprated:
+                    selectedFragment = new TopRatedFragment();
+                    break;
+                case R.id.nav_popular:
+                    selectedFragment = new PopularFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        }
+    };
 }
