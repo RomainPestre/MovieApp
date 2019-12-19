973940445b8dad95c1fe1b1310f8d680ccf7f80d
RomainPestre <rompste@gmail.com>
2019-12-19 15:10:44 +0100

    Improvement of the Recycler View design (addition of the poster)

--- app/src/main/java/com/example/movieapp/adapter/MoviesAdapter.java
+++ app/src/main/java/com/example/movieapp/adapter/MoviesAdapter.java
@@ -1,17 +1,27 @@
 package com.example.movieapp.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.movieapp.R;
 import com.example.movieapp.model.Movie;
 import com.example.movieapp.model.OnClickMovie;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
@@ -27,6 +37,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         public TextView data;
         public TextView movieDescription;
         public TextView rating;
+        public ImageView thumbnail;
 
 
         public MovieViewHolder(View v) {
@@ -36,6 +47,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
             data = (TextView) v.findViewById(R.id.subtitle);
             movieDescription = (TextView) v.findViewById(R.id.description);
             rating = (TextView) v.findViewById(R.id.rating);
+            thumbnail = (ImageView) v.findViewById(R.id.thumbnail);
         }
     }
 
@@ -53,7 +65,6 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         return new MovieViewHolder(view);
     }
 
-
     @Override
     public void onBindViewHolder(MovieViewHolder holder, final int position) {
         final Movie movieCurent = movies.get(position);
@@ -61,6 +72,9 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         holder.data.setText(movies.get(position).getReleaseDate());
         holder.movieDescription.setText(movies.get(position).getOverview());
         holder.rating.setText(movies.get(position).getVoteAverage().toString());
+        RequestOptions requestOptions = new RequestOptions().centerCrop().placeholder(R.drawable.loading_shape).error(R.drawable.loading_shape);
+        String urlPic = "https://image.tmdb.org/t/p/w500" + movies.get(position).getPosterPath();
+        Glide.with(context).load(urlPic).apply(requestOptions).into(holder.thumbnail);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
